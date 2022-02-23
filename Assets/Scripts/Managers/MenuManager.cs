d9a1f5b3d294aad8c0c3069717f0ffec1f343e7b
Francisco Castañeda <dragalichi@gmail.com>
2022-02-23 06:34:14 -0600

    Add Main Menu and Connect to Game Scene

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] Button playButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(() => ChangeSceneByIndex(1));    
+    }
+
+    void ChangeSceneByIndex(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
